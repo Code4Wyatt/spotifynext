4b3501736e8e02bf65d30ccb7b26bcb9f6c41e75
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-01-05 12:49:57 +0000

    Added NextAuth w/ Spotify auth token

---
+++ my-app/pages/index.js
@@ -0,0 +1,15 @@
+import Sidebar from "../components/Sidebar";
+
+export default function Home() {
+  return (
+    <div className="">
+
+      <main className="bg-black h-screen overflow-hidden">
+        <Sidebar />
+        {/* Center */}
+      </main>
+
+      <div>{/* Player */}</div>
+    </div>
+  );
+}
