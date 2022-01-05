b9955113c9322f2831da0e0ec202811e542fdbcd
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-01-05 22:05:59 +0000

    Updated player

--- my-app/components/Player.js
+++ my-app/components/Player.js
@@ -1,6 +1,6 @@
 import useSpotify from "../hooks/useSpotify";
 import useSongInfo from "../hooks/useSongInfo";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
@@ -14,11 +14,36 @@ function Player() {
 
     const songInfo = useSongInfo();
 
+    const fetchCurrentSong = () => {
+        if (!songInfo) {
+            spotifyApi.getMyCurrentPlayingTrack().then((data) => {
+                console.log("Now playing: ", data.body?.item);
+                setCurrentIdTrack(data.body?.item?.id);
+
+                spotifyApi.getMyCurrentPlaybackState().then((data) => {
+                    setIsPlaying(data.body?.is_playing);
+                });
+            });
+        }
+    };
+
+    useEffect(() => {
+        if (spotifyApi.getAccessToken() && !currentTrackId) {
+            fetchCurrentSong();
+            setVolume(50);
+        }
+    }, [currentTrackId, spotifyApi, session]);
+
     return (
-        <div>   
+        <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">   
             {/* Left */}
+            <div className="flex items-center space-x-4">
+                <img className="hidden md:inline h-10 w-10" src={songInfo?.album.images?.[0]?.url} alt="" />
+            
             <div>
-                <img src={songInfo?.album.images?.[0]?.url} alt="" />
+                <h3>{songInfo?.name}</h3>
+                <p>{songInfo?.artists?.[0]?.name}</p>
+            </div>
             </div>
         </div>
     )
